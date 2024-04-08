01c40ad53d309bf618f590ca667a5b05cb79d10d
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-04-08 08:32:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6728160367"
-SUPPORT_GRP = "shizukamusicupdates"
-UPDATE_CHNL = "shizukamusicupdates"
-OWNER_USERNAME = "DFSchinna_op"
+OWNER_ID = "6439286014"
+SUPPORT_GRP = "DfsNetwork_OP"
+UPDATE_CHNL = "DfsNetwork_OP"
+OWNER_USERNAME = "DFSchinna"
 
