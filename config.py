ec9731d6099733ed58e608eb1b50505d6439fc7a
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-25 13:55:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = "6439286014"
-SUPPORT_GRP = "DfsNetwork_OP"
-UPDATE_CHNL = "DfsNetwork_OP"
-OWNER_USERNAME = "DFSchinna"
+SUPPORT_GRP = "BTS_network_op"
+UPDATE_CHNL = "BTS_network_op"
+OWNER_USERNAME = "BTSchinna"
 
