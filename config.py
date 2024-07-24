181ba1f06464e4429d0905e159e3a5734296880a
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-07-25 00:08:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6439286014"
+BOT_TOKEN = getenv("BOT_TOKEN", "6966314106:AAF_vby-qjxmnguyJmVz8q8zkBs5QsM5Xbk")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = "6896043885"
 SUPPORT_GRP = "DFSchinnaop"
 UPDATE_CHNL = "DFSchinnaop"
 OWNER_USERNAME = "DFSchinnaop"
