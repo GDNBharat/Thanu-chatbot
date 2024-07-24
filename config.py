7d7656ac265a136f53c54010b726cb9648e5acbe
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-07-25 00:04:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = "6439286014"
-SUPPORT_GRP = "BTS_network_op"
-UPDATE_CHNL = "BTS_network_op"
-OWNER_USERNAME = "BTSchinna"
+SUPPORT_GRP = "DFSchinnaop"
+UPDATE_CHNL = "DFSchinnaop"
+OWNER_USERNAME = "DFSchinnaop"
 
