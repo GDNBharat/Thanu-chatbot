7d307aa1ede5a30200976065ef76b3bb99fb128f
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-08-09 11:28:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 API_ID = "6435225"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "34f9cb93364db16fc45d003e4c81d97a"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "6966314106:AAF_vby-qjxmnguyJmVz8q8zkBs5QsM5Xbk")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority")
