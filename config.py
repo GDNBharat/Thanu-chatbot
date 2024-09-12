31c90a90f473ad627a354073696dcae76ca09646
GDN Bharath <dn6418138@gmail.com>
2024-09-12 11:13:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = "28362850"
 API_HASH = "34f9cb93364db16fc45d003e4c81d97a"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "6966314106:AAF_vby-qjxmnguyJmVz8q8zkBs5QsM5Xbk")
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://VamsixD:VamsixD@vamsi.x7gyybv.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = "6896043885"
 SUPPORT_GRP = "DFSchinnaop"
 UPDATE_CHNL = "DFSchinnaop"
