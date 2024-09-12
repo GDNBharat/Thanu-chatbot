72689712ce4286ec187e0ed796339a51a2613adb
GDN Bharath <dn6418138@gmail.com>
2024-09-12 11:15:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "28362850"
 # -------------------------------------------------------------
 API_HASH = "34f9cb93364db16fc45d003e4c81d97a"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "6966314106:AAF_vby-qjxmnguyJmVz8q8zkBs5QsM5Xbk")
+BOT_TOKEN = getenv("BOT_TOKEN", "6966314106:AAFtyEd7_WxgV7hKmfA6u7gzwV5FFVpLuW0")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://VamsixD:VamsixD@vamsi.x7gyybv.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = "6896043885"
 SUPPORT_GRP = "DFSchinnaop"
