5fddb46a613a426482781761f15aed534adf64a5
Chinna op <opchinna742@gmail.com>
2024-12-26 10:02:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ API_ID = "28362850"
 # -------------------------------------------------------------
 API_HASH = "34f9cb93364db16fc45d003e4c81d97a"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "6966314106:AAFtyEd7_WxgV7hKmfA6u7gzwV5FFVpLuW0")
+BOT_TOKEN = getenv("BOT_TOKEN", "7973015894:AAHXyY4TDKQY7JK1FAK1HkoyOvQoaT2WKm4")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://VamsixD:VamsixD@vamsi.x7gyybv.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = "6896043885"
-SUPPORT_GRP = "DFSchinnaop"
-UPDATE_CHNL = "DFSchinnaop"
+SUPPORT_GRP = "Thanuupdates"
+UPDATE_CHNL = "Thanuupdates"
 OWNER_USERNAME = "DFSchinnaop"
 
